8ba693bd26dcf81eede59dccb7f0314a7bd71bed
Nick Sette <njs2394@gmail.com>
2023-11-07 10:42:38 -0500

    Question 1

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  console.log("Hello")
+
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => {
+      console.log("Hi")
+    })
+
+  console.log("Sup?")
+
+  //ORDER WOULD BE
+  //  Hello, Sup, Hi because fetch is async and will take longer when the api
+
+
 });
